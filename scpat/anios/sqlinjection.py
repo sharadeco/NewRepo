142d27888da68ea697b5d27419607fd25bc3ccf2
Anios123 <91587014+Anios123@users.noreply.github.com>
2022-01-18 19:34:07 +0530

    Update sqlinjection.py

--- scpat/anios/sqlinjection.py
+++ scpat/anios/sqlinjection.py
@@ -335,10 +335,10 @@ def update_records(column, table):
                     #"dbo.[Anios_DemandData].[Date]  !< DATEADD(MONTH, DATEDIFF(MONTH, 0,@datevar_CET)-12, 0)  AND "
                     # +"DATEPART(month, dbo.[Anios_DemandData].[Date])		    >= datepart(month,@datevar_CET)- 1) AND  "
                     +"dbo.[Anios_DemandData].[Delete_Ind] = 'F'	 ")            
-            #sqlD = str( sql_timezone                    
-            #        +"Delete from dbo.[Anios_DemandData] where [Date]  >=DATEADD(MONTH, DATEDIFF(MONTH, 0,@datevar_CET)-12, 0)")
-            #db.session.execute(sqlD)
-            #db.session.commit()
+            sqlD = str( sql_timezone                    
+                    +"Delete from dbo.[Anios_DemandData] where [Date]  >=DATEADD(MONTH, DATEDIFF(MONTH, 0,@datevar_CET)-12, 0)")
+            db.session.execute(sqlD)
+            db.session.commit()
 
         if table == "Forecast":
             sql = str( sql_timezone
