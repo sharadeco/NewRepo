a4203b73d5b6aea09445f0211b3c568028dbe9fe
sharadeco <84703518+sharadeco@users.noreply.github.com>
2022-03-29 12:05:43 +0530

    Update extensions.py

--- scpat/anios/extensions.py
+++ scpat/anios/extensions.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_material import Material
 from flask_login import LoginManager, login_user
-
 import ray
 
 # from flask_login.mixins import UserMixin
