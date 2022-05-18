3d361df7ecabc59d7db7d74d00fdfba86dd7f09d
Anios123 <91587014+Anios123@users.noreply.github.com>
2022-05-18 23:10:19 +0530

    Update forecastmodel.py

--- scpat/anios/forecastmodel.py
+++ scpat/anios/forecastmodel.py
@@ -281,8 +281,8 @@ def runmodel(data,New_data):
 
         df = New_data[''].astype(str).str.split("-", expand=True)
     
-        start_date=datetime.now().replace(day=1).replace(minute=00, hour=00, second=00)
-        end_date=datetime.now().replace(day=1).replace(minute=00, hour=00, second=00) + relativedelta(months=21)
+        start_date=datetime.datetime.now().replace(day=1).replace(minute=00, hour=00, second=00)
+        end_date=datetime.datetime.now().replace(day=1).replace(minute=00, hour=00, second=00) + relativedelta(months=21)
     
         list_date=[]
         for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
